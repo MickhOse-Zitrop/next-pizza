3ffed9ac1b6a62bad21d8560ea3fc87f3608e6ad
MickhOse Zitrop <mickhose-zitrop@yandex.ru>
2025-04-22 20:52:38 +0300

    Update page.tsx

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,16 +1,25 @@
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
-import {
-  Container,
-  GroupVariants,
-  ProductImage,
-  Title,
-} from "@/components/shared";
+import { ChooseProductModal } from "@/shared/components/shared";
 
-export default async function ProductPage({
+export default async function ProductModalPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>123123</h1>;
+  const product = await prisma.product.findFirst({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      ingredients: true,
+      items: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return <ChooseProductModal product={product} />;
 }
