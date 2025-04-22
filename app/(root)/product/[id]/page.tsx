a3933881169b15e7248be517212edb6df9469464
MickhOse Zitrop <mickhose-zitrop@yandex.ru>
2025-04-22 20:53:49 +0300

    Update page.tsx

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,18 +1,29 @@
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
-import {
-  Container,
-  GroupVariants,
-  ProductImage,
-  Title,
-} from "@/components/shared";
+import { Container, ProductForm } from "@/shared/components/shared";
+import React from "react";
 
 export default async function ProductPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+  const product = await prisma.product.findFirst({
+    where: { id: Number(id) },
+    include: {
+      ingredients: true,
+      category: {
+        include: {
+          products: {
+            include: {
+              items: true,
+            },
+          },
+        },
+      },
+      items: true,
+    },
+  });
 
   if (!product) {
     return notFound();
@@ -20,44 +31,7 @@ export default async function ProductPage({
 
   return (
     <Container className="flex flex-col my-10">
-      <div className="flex flex-1">
-        <ProductImage
-          imageUrl={product.imageUrl}
-          name={product.name}
-          size={40}
-        />
-        <div className="w-[490px] bg-[#F7F6F5] p-7">
-          <Title
-            text={product.name}
-            size="md"
-            className="font-extrabold mb-1"
-          />
-          <p className="text-gray-400">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
-            architecto asperiores aspernatur dicta distinctio ea eligendi error
-            fugit illum incidunt laboriosam quisquam saepe sed temporibus ullam
-            voluptas voluptatem, voluptates voluptatibus?
-          </p>
-
-          <GroupVariants
-            selectedValue="2"
-            items={[
-              {
-                name: "Маленькая",
-                value: "1",
-              },
-              {
-                name: "Средняя",
-                value: "2",
-              },
-              {
-                name: "Большая",
-                value: "3",
-              },
-            ]}
-          />
-        </div>
-      </div>
+      <ProductForm product={product} />
     </Container>
   );
 }
